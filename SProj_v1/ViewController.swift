e87f56d49681f731654470e86dc31b1dec46e507
Eugene <evgen29031999@bk.ru>
2021-11-20 23:52:06 +0300

    Button_Alert_Constraints_Counter

--- SProj_v1/ViewController.swift
+++ SProj_v1/ViewController.swift
@@ -9,6 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var counter = 0;
+    
+    
+    @IBAction func buttonControl(_ sender: Any) {
+        counter += 1;
+        
+        let alert1 = UIAlertController(title: "Четность", message: "Четное - \(counter)", preferredStyle: UIAlertController.Style.alert);
+        let alert2 = UIAlertController(title: "Четность", message: "Нечетное  - \(counter)", preferredStyle: UIAlertController.Style.alert);
+        
+        let alertAction1 = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil)
+        let alertAction2 = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil)
+        
+        if counter%2 == 0 {
+            alert1.addAction(alertAction1)
+            present(alert1, animated: true, completion: nil)
+        }else{
+            alert2.addAction(alertAction2)
+            present(alert2, animated: true, completion: nil)
+
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
