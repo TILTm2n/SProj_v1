42748956313800cfbb9162f83e49bab77cad8ffc
Eugene <evgen29031999@bk.ru>
2021-11-20 12:20:28 +0300

    Initial Commit

---
+++ SProj_v1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SProj_v1
+//
+//  Created by Eugene on 20.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
