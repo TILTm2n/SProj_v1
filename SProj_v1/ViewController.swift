a8f556864b90be1992b9b8d3cda83b80af1f27aa
Eugene <evgen29031999@bk.ru>
2021-11-21 16:45:07 +0300

    С_TV_2

--- SProj_v1/ViewController.swift
+++ SProj_v1/ViewController.swift
@@ -9,33 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var counter = 0;
+    let names = ["Курица","Свинина","Говядина","Окунь","Осетр","Камбала","Ставридка","Стерлядь","Устрицы","Оленина","Заяц","Индейка",]
     
+    //override var prefersStatusBarHidden: Bool{
+    //    return true
+    //}
     
-    @IBAction func buttonControl(_ sender: Any) {
-        counter += 1;
-        
-        let alert1 = UIAlertController(title: "Четность", message: "Четное - \(counter)", preferredStyle: UIAlertController.Style.alert);
-        let alert2 = UIAlertController(title: "Четность", message: "Нечетное  - \(counter)", preferredStyle: UIAlertController.Style.alert);
-        
-        let alertAction1 = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil)
-        let alertAction2 = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil)
-        
-        if counter%2 == 0 {
-            alert1.addAction(alertAction1)
-            present(alert1, animated: true, completion: nil)
-        }else{
-            alert2.addAction(alertAction2)
-            present(alert2, animated: true, completion: nil)
-
-        }
-        
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+}
 
-
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = names[indexPath.row]
+        cell?.imageView?.image = UIImage(named: "avatar")
+        cell?.backgroundColor = UIColor(named: "Red")
+        cell?.layer.cornerRadius = 10
+        
+        
+        return cell!
+    }
+    
+    
 }
 
